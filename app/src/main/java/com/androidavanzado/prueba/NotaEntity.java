49f3c770ed7987435616914a3599ac748c7a0aa5
is-lgarcia <luis10.g@hotmail.com>
2020-05-16 22:32:43 -0600

    firts commit

---
+++ app/src/main/java/com/androidavanzado/prueba/NotaEntity.java
@@ -0,0 +1,62 @@
+package com.androidavanzado.prueba;
+
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notas")
+public class NotaEntity {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public String titulo;
+    public String contenido;
+    public boolean favorita;
+    public int color;
+
+    public NotaEntity(String titulo, String contenido, boolean favorita, int color) {
+        this.titulo = titulo;
+        this.contenido = contenido;
+        this.favorita = favorita;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public boolean isFavorita() {
+        return favorita;
+    }
+
+    public void setFavorita(boolean favorita) {
+        this.favorita = favorita;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
